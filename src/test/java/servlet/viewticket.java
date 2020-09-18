f351aa12ef1accfd93e2209e46aa56feeeb959f1
mntswaroop <47464540+mntswaroop@users.noreply.github.com>
2020-09-17 20:42:04 -0700

    Update viewticket.java
    updated IP

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://34.82.84.41:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://34.83.255.6:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
